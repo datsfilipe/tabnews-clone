026f99106ca0fce7ea8c1bb4ef044995076c8c00
datsfilipe <datsfilipe.foss@proton.me>
2023-07-02 05:39:53 -0300

    style(tailwind): add brand color

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#24292f",
+      },
+    },
   },
   plugins: [],
 };
