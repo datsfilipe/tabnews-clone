f9960f4a7857758ae2dbeb23c21fc14ab834c89a
datsfilipe <datsfilipe.foss@proton.me>
2023-07-04 14:51:35 -0300

    feat(web): add relative timestamp to post item

--- web/src/components/List/PostItem.tsx
+++ web/src/components/List/PostItem.tsx
@@ -7,6 +7,47 @@ type Props = {
   index: number;
 };
 
+const timeAgo = (timestamp: string) => {
+  const date = new Date(timestamp);
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+
+  if (seconds < 60) {
+    return new Intl.RelativeTimeFormat("pt-BR", {
+      style: "long",
+      numeric: "auto",
+    }).format(-seconds, "second");
+  } else if (minutes < 60) {
+    return new Intl.RelativeTimeFormat("pt-BR", {
+      style: "long",
+      numeric: "auto",
+    }).format(-minutes, "minute");
+  } else if (hours < 24) {
+    return new Intl.RelativeTimeFormat("pt-BR", {
+      style: "long",
+      numeric: "auto",
+    }).format(-hours, "hour");
+  } else if (hours < 30 * 24) {
+    return new Intl.RelativeTimeFormat("pt-BR", {
+      style: "long",
+      numeric: "auto",
+    }).format(-Math.floor(hours / 24), "day");
+  } else if (hours < 365 * 24) {
+    return new Intl.RelativeTimeFormat("pt-BR", {
+      style: "long",
+      numeric: "auto",
+    }).format(-Math.floor(hours / (24 * 30)), "month");
+  } else {
+    return new Intl.RelativeTimeFormat("pt-BR", {
+      style: "long",
+      numeric: "auto",
+    }).format(-Math.floor(hours / (24 * 365)), "year");
+  }
+};
+
 export function PostItem(props: Props) {
   return (
     <div className="flex flex-col pb-2 px-4">
@@ -22,6 +63,7 @@ export function PostItem(props: Props) {
           <a href="#">{props.author}</a>
           {/* should be in the format: 2 hours ago, 6 days ago, something like that*/}
           &nbsp;
+          {`· ${timeAgo(props.timestamp)}`}
         </span>
       </div>
     </div>
