d9c01a00a747d6767c772b6709348123b03c20a0
datsfilipe <datsfilipe.foss@proton.me>
2023-07-04 14:51:11 -0300

    style(web): update post item paddings, text size, etc

--- web/src/components/List/PostItem.tsx
+++ web/src/components/List/PostItem.tsx
@@ -9,18 +9,20 @@ type Props = {
 
 export function PostItem(props: Props) {
   return (
-    <div className="flex flex-col p-4">
+    <div className="flex flex-col pb-2 px-4">
       <div className="flex flex-row">
-        <h1 className="text-2xl font-bold space-x-2">
-          <span>{props.index}</span>
+        <h1 className="text-[1.05rem] space-x-2">
+          <span>{props.index}.</span>
           <span>{props.title}</span>
         </h1>
-        <div className="flex flex-row ml-4">
-          <span className="text-sm text-gray-400">
-            {`${props.coins} tabcoins • ${props.comments} comments •`}&nbsp;
-            <a href="#">{props.author}</a>&nbsp;{`• ${props.timestamp}`}
-          </span>
-        </div>
+      </div>
+      <div className="flex flex-row ml-4">
+        <span className="text-[0.78rem] text-gray-500">
+          {`${props.coins} tabcoins · ${props.comments} comments ·`}&nbsp;
+          <a href="#">{props.author}</a>
+          {/* should be in the format: 2 hours ago, 6 days ago, something like that*/}
+          &nbsp;
+        </span>
       </div>
     </div>
   );
